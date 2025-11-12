6821006812d94101cf6bdf698f9a5b79de6cf347
NomisIV <simon@nomisiv.com>
2025-11-12 15:19:29 +0100

    Use the rust version from rust-toolchain

--- flake.nix
+++ flake.nix
@@ -3,22 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = with pkgs; [
-        rustc
-        cargo
-        rust-analyzer
-        rustfmt
-        nil
-      ];
+  outputs = inputs:
+    with inputs; let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [(import rust-overlay)];
+      };
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchains.toml;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          toolchain
+          cargo
+          rust-analyzer
+          rustfmt
+          nil
+        ];
+      };
     };
-  };
 }
